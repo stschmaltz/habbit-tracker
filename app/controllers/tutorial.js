1b962c0348195d4172a5cc96d31edc6190586585
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-27 21:03:58 -0700

    hook up firebase, learn about models

--- app/controllers/tutorial.js
+++ app/controllers/tutorial.js
@@ -3,23 +3,23 @@ import Controller from '@ember/controller';
 import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
+  headerMessage: 'Coming Soon',
   emailAddress: '',
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
   isDisabled: not('isValid'),
 
   actions: {
-    saveInvitation() {
-      alert(
-        `Saving of the following email address is in progress: ${this.get(
-          'emailAddress',
-        )}`,
-      );
+    async saveInvitation() {
+      const email = this.emailAddress;
+
+      const newInvitation = this.store.createRecord('invitation', { email });
+
+      const response = await newInvitation.save();
       this.set(
         'responseMessage',
-        `Thank you! We've just saved your email address: ${this.get(
-          'emailAddress',
-        )}`,
+        `Thank you! We saved your email address with the following id:
+        ${response.id}`,
       );
       this.set('emailAddress', '');
     },
